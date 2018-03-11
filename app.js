6f7a5ae1d7326520ae70667c280432dcecd6c717
Sumeet <sj.jainsumeet@gmail.com>
2018-03-10 20:31:47 -0600

    adding 1st api

--- app.js
+++ app.js
@@ -3,46 +3,13 @@ const app = express();
 const config = require('./config');
 const { buildSchema } = require('graphql');
 const graphqlHTTP = require('express-graphql');
+const user = require('./api/user')
 
 
 // static folder
 app.use(express.static('public'));
 
-<<<<<<< HEAD
-//test push Sumeet
-
-// examples//
-=======
-// Examples ////
->>>>>>> 02f8fae9997b45cf06f76f4d50e04211684fe1fd
-/**
- * https://codesandbox.io/s/jvlrl98xw3
- * https://github.com/codediger/graphql-mutation-example
- */
-
-
-// /* Here a simple schema is constructed using the GraphQL schema language. 
-//    More information can be found in the GraphQL spec release */
-// let schema = buildSchema(`
-//   type Query {
-//     postTitle: String,
-//     blogTitle: String
-//   }
-// `);
-
-// // Root provides a resolver function for each API endpoint
-// let root = {
-//   postTitle: () => 'Build a Simple GraphQL Server With Express and NodeJS',
-//   blogTitle: () => 'scotch.io'
-// };
-
-// app.use('/', graphqlHTTP({
-//   schema: schema,
-//   rootValue: root,
-//   graphiql: true//Set to false if you don't want graphiql enabled
-// }));
-
-
-
+// Routes setup
+require('./api/routes')(app);
 
 app.listen(config.port, _ => console.log(`Listening on ${config.port}`))
