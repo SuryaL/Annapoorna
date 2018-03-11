5d56c8a7a4d7e181b3aab43d40c8a0325f7dfae1
Yeshwanth <yeshwanth.malekar@gmail.com>
2018-03-10 23:07:28 -0600

    code clean up

--- api/user/user.controller.js
+++ api/user/user.controller.js
@@ -9,15 +9,8 @@ const create = async function(req, res, next) {
 
 const find = async function(req, res, next) {
     let query = 'SELECT * FROM user';
-    // client.execute(query, [])
-    //     .then(function(resp) {
-    //         res.send(200, resp.rows);
-    //     })
-    //     .catch(err => console.log("err:", err))
-
     let result = await executeQuery(query, []);
-    res.status(200).send(result);
-
+    res.status(200).send(result.rows);
 }
 
 const update = async function(req, res, next) {
--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const config = require('./config');
-const { buildSchema } = require('graphql');
-const graphqlHTTP = require('express-graphql');
 const cassandra   = require ('./boundaries/cassandra');
+// Cassandra setup
 cassandra.init(config.cassandra).then(function () {
   try {
+	// Routes setup
 	require('./api/routes')(app);
   } catch (e) {
     console.log ('\033[31m' + e.stack + '\033[0m');
@@ -15,6 +15,5 @@ cassandra.init(config.cassandra).then(function () {
 // static folder
 app.use(express.static('public'));
 
-// Routes setup
 
 app.listen(config.port, _ => console.log(`Listening on ${config.port}`))
