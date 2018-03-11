f42691f547c475e5c72cb702b2edbad19d8d0a14
Sravanthi Gujjula <sravanthigujjula@Sravanthis-MacBook-Air.local>
2018-03-10 22:01:17 -0600

    faker modified

--- api/user/user.controller.js
+++ api/user/user.controller.js
@@ -1,6 +1,28 @@
 const service = require('./user.service');
 const client = require('../../boundaries/cassandra').client;
+const faker = require('./../../faker.js');
 const create = async function(req, res, next) {
+
+    
+
+
+}
+
+function createFakeData () {
+
+    console.log(faker.getUser());
+    let user = faker.getUser();
+    const columns = [];
+    const params = [];
+    const questions = [];
+    Object.keys(user).forEach(key => {
+        columns.push(key);
+        params.push(user[key]);
+        questions.push('?');
+    })
+
+    // const query = 'INSERT INTO user (' + columns.join() + ') VALUES (' + questions.join() + ')';
+    // let result = yield executeQuery(query, params)
 
 
 }
@@ -22,6 +44,7 @@ const update = async function(req, res, next) {
 const remove = async function(req, res, next) {
 
 };
+
 
 
 module.exports = {
