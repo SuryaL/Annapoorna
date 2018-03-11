7cb6e9e42560c618f688619e21b34233aef71fcc
Surya <surysunny17@gmail.com>
2018-03-11 00:41:29 -0600

    removed

--- api/routes.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app){
-    require('./user')(app);
-    require('./menu')(app);
-    require('./order')(app);
-    require('./vote')(app);
-    require('./status')(app);
-}
