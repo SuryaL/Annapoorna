7d85f4289f24e83182209c07fec8d8fca01e8269
Yeshwanth <yeshwanth.malekar@gmail.com>
2018-03-11 00:24:33 -0600

    menu apis & routes updates

--- api/menu/menu.controller.js
+++ api/menu/menu.controller.js
@@ -1,9 +1,28 @@
-const create = async function(req){
+const client = require('../../boundaries/cassandra').client;
+const executeQuery = require('../utils').execQuery;
+const MenuService = require('./menu.service');
+const _ = require('underscore');
 
+const create = async function(req){
+	const body = _.clone(req.body);
+	if (body.id != null) {
+	    delete body.id;
+	}
+	
+	//TODO : verify all params body
+	    
+	Object.assign(body, MenuService.createNewMenuData());
+	
+	const menu = await MenuService.createMenu(body);
+	return menu
+	console.log(err);        
 }
 
 const find = async function(req){
-    
+    const query = _.clone(req.query);
+    const result = await MenuService.getMenu(query);
+    return result
+    console.log(err);
 }
 
 const update = async function(req){
