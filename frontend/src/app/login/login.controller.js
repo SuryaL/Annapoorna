3dcb215a6e033d416fc9fa2c22ac4556e8d7cbe3
Sravanthi Gujjula <sravanthigujjula@Sravanthis-MacBook-Air.local>
2018-03-11 20:49:34 -0500

    login modified

--- frontend/src/app/login/login.controller.js
+++ frontend/src/app/login/login.controller.js
@@ -3,13 +3,16 @@ class loginCtrl {
         'ngInject';
         Object.assign(this, { $state, oauthService });
         this.user = {};
+        this.$state = $state;
     }
 
     submit() {
     	console.log(this.user);
     	this.oauthService.open()
     		.then(resp => {
-    			console.log("fb resp", resp);
+                console.log("fb resp", resp);
+                this.$state.go('app.home');
+                
     		})
     		.catch(err => console.log("fb err", err));
     }
