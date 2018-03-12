30a9075782cbbc3d3c90bc79e22f7a164c4d1d0d
Yeshwanth <yeshwanth.malekar@gmail.com>
2018-03-11 21:32:26 -0500

    service injection fix

--- frontend/src/common/components/vote_item/vote_item.js
+++ frontend/src/common/components/vote_item/vote_item.js
@@ -1,11 +1,11 @@
 import angular from 'angular';
 import uirouter from 'angular-ui-router';
 import component from './vote_item.component';
-import OauthService from 'common/services/oauth';
+import AuthService from 'common/services/auth';
 
 const module = angular.module('voteItem', [
         uirouter,
-        OauthService
+        AuthService
     ])
 
     .config(function($stateProvider) {
