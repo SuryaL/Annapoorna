63a8236f013e235da19273464fb266f8e534effc
Surya <surysunny17@gmail.com>
2018-03-11 21:11:40 -0500

    added simple components

--- frontend/src/common/components/simpleHeader/simpleHeader.controller.js
+++ frontend/src/common/components/simpleHeader/simpleHeader.controller.js
@@ -2,11 +2,6 @@ class voteCtrl {
     constructor($state, oauthService) {
         'ngInject';
         this.user = {};
-        this.headTitle = 'Vote for this weeks dishes';
-        this.subheadTitle = '8.8.88';
-        this.voteItems=[
-            1,2,3,4,5
-        ]
     }
 
     submit() {
