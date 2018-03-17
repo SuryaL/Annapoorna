3486585ff34d06d4988316df46bef09b17335493
Surya <surysunny17@gmail.com>
2018-03-16 21:27:12 -0500

    gulp component generator added

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,9 @@
 const gulp = require('gulp');
-const uglify = require('gulp-uglify');
-
 const yargs = require('yargs');
 const rename = require('gulp-rename');
 const template = require('gulp-template');
 const path = require('path');
-/*
-gulp.task - define task
-gulp.src - point to files to use
-gulp.dest - points to folder to output
-gulp.watch - watch files and folders to changes
 
-*/
 
 const cap = (name) => {
   array = name.toLowerCase().split('_');
@@ -31,24 +23,21 @@ const tag = (val) => {
 
 const blankComponentPath = path.join(__dirname, 'generator', '/component/**/*.**');
 
-gulp.task('message', () => console.log('Gulp running'));
-
-gulp.task('default', () => console.log('Gulp default with no args'));
-
-gulp.task('watch', () => {
-
-})
-
 let srcPath = path.join(__dirname,'frontend','src');
 let commonPath = path.join(__dirname,'frontend','src','common');
+let commonComponentsPath =  path.join(commonPath,'components')
 let appPath = path.join(__dirname,'frontend','src','app');
+
+
 gulp.task('component', () => {
   let name = yargs.argv.name;
   let parent = yargs.argv.parent;
+  let parentArray = (parent||'').split('/');
   let app = yargs.argv.app;
-  // let destPath = app == !!app && app=='app' ? path.join(appPath,)
-  
-  gulp.src(blankComponentPath)
+  let basePath = app == !!app && app=='app' ? appPath : commonComponentsPath;
+  let destPath = path.join(basePath,...parentArray, name);
+
+  return gulp.src(blankComponentPath)
     .pipe(template({
       name: name,
       upCaseName: cap(name),
@@ -56,8 +45,8 @@ gulp.task('component', () => {
       tagName: tag(name)
     }))
     .pipe(rename((p)=>{
-      p.basename =p.basename.replace('temp',name);
+      p.basename = p.basename.replace('temp',name);
     }))
-    .pipe(gulp.dest())
+    .pipe(gulp.dest(destPath))
 
 })
