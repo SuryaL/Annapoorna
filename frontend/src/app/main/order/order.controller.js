db710a3b953423418a7dcd3309d12f38bfdf4e6a
Surya <surya.c@seventablets.com>
2018-03-26 17:41:14 -0500

    started orders page

--- frontend/src/app/main/order/order.controller.js
+++ frontend/src/app/main/order/order.controller.js
@@ -1,7 +1,7 @@
 class OrderController {
-    constructor($state, $auth, VoteService) {
+    constructor($state, $auth,MenuService,VoteService,StatusService,$q,MenuVotingLimit, MyToastr) {
         'ngInject';
-        Object.assign(this, { $state, $auth, VoteService });
+        Object.assign(this, { $state, $auth, MenuService,VoteService,StatusService,$q,MenuVotingLimit, MyToastr });
         this.user = {};
         this.headTitle = 'Order this week\'s dishes';
 
@@ -21,12 +21,50 @@ class OrderController {
             { "itemName": "Gutkowski-Bahringer", "isSelected": false },
             { "itemName": "Baumbach Inc", "isSelected": false }]
         this.btnText = "order";
-        this.getVotedItems();
-        this.order_deadline = '03-20-2018';
+        // this.getVotedItems();
+        // this.order_deadline = '03-20-2018';
+        this.init();
     }
 
-    get subheadTitle(){
-        return 'Deadline : ' + this.order_deadline.replace(/-/g,'.')
+    init(){
+        this.$q.all([this.StatusService.findActiveWeek(), this.MenuService.find()])
+        .then(results => {
+            this.weekDetails = results[0]||{};
+            this.currentWeek = this.weekDetails.week;
+            this.voting_status = this.weekDetails.voting_status;
+            this.order_status = this.weekDetails.order_status;
+            this.order_deadline = this.weekDetails.order_deadline;
+            this.vote_deadline = this.weekDetails.voting_deadline;
+            this.menuItems = results[1] || [];
+            
+            return this.$q.all([
+                this.VoteService.find({week:this.weekDetails.week}),
+                // this.$q(resolve=>resolve(['39813b97-4b16-434b-bcf5-e9080e7565f8']))
+                //TODO: Add Voting results api 
+                // majority is current top dishes
+                this.VoteService.getMajority({week:this.weekDetails.week}),
+            ])
+        })
+        .then(([currentWeekVotes, majority])=> {
+            this.majority = new Set(majority||[]);
+            const currentWeekUserVote = (currentWeekVotes||[])[0]||{};
+            const dishes_voted = currentWeekUserVote.dishes||[];
+            this.already_voted = !!dishes_voted.length;
+            // (dishes_voted||[]).forEach(dish_id => this.selectedItems.add(dish_id));
+        })
+        .catch(console.error)
+    }
+
+    get subheadTitle() {
+        let d = '';
+        if(this.order_deadline){
+            d = new Date(this.order_deadline).toLocaleString().split('T')[0]
+        }
+        return 'Deadline : ' + d
+    }
+
+    showSubmit(){
+        return this.currentWeek && !this.timePassed && !!this.voting_status && !this.order_status
     }
 
     orderClicked = () => {
