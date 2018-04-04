a8c6228b0a1be4b8d63ab716d377544e5565799d
Surya <surya.c@seventablets.com>
2018-04-04 10:42:40 -0500

    moved configs to env

--- config/index.js
+++ config/index.js
@@ -27,9 +27,5 @@ module.exports = {
 
     // List of user roles
     roles: ['user', 'cook', 'admin'],
-
-    TOKEN_SECRET: 'k?@Zr4=mpC^RMdLaU@sh68FdKMJ+wBTky+pQ*pxjpHjq#wz6YWc2U6eyNXEvmSNzbKGY98Wr8rn*XS-_8zSyNFkC?fu7eK8L5UtxjPgeR_#nR3Z%qM$%sKy3H-edpL+cSpD2aVZGyLrj^Yq4Ffe4QuLcbuSc8K=tXL7T%D+5%-6CwJ9Pm+QX2ygbk^=K5!YmUMyFhrta7qwHfa@L+2e98q7$yHu%rGfu5RaCAB$&NGZ5zD7sTkFH_-zbU_Xm=Dab',
-    // OAuth 2.0
-    FACEBOOK_CLIENT_ID: '1653283634989097',
-    FACEBOOK_SECRET: 'b58070df68e4a33cba313c0431654fab', //
+   ...env.secret_configs
 }
