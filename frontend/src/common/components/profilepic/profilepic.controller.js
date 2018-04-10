074cc5591b89c590051335fd33a72e74b7bb8d61
Surya <surysunny17@gmail.com>
2018-04-09 22:21:41 -0500

    profile pic sce

--- frontend/src/common/components/profilepic/profilepic.controller.js
+++ frontend/src/common/components/profilepic/profilepic.controller.js
@@ -1,8 +1,18 @@
 class ProfilepicController {
-    constructor() {
+    constructor($sce) {
         'ngInject';
         this.name = 'Profilepic';
+        this.$sce = $sce;
         // this.defaultPic = require('~assets/images/blank_profile.png');
+    }
+
+    get imageurl(){
+        if(this.image){
+            return this.$sce.trustAsResourceUrl(this.image)
+        }
+        else{
+            return '';
+        }
     }
 }
 
